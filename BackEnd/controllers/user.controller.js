e0c61879371b6b6ac077c7f1b8cada839d275a21
raziq0835 <raziq0835@gmail.com>
2025-03-02 10:31:20 +0530

    Tested the register router

--- BackEnd/controllers/user.controller.js
+++ BackEnd/controllers/user.controller.js
@@ -1,30 +1,25 @@
-require('dotenv').config();
-const userModel = require("../models/user.model")
-const userServices = require('../services/user.services')
-const {validationResult} = require('express-validator')
+require("dotenv").config();
+const userModel = require("../models/user.model");
+const userServices = require("../services/user.services");
+const { validationResult } = require("express-validator");
 
-exports.userRegister = async function(req , res, nest){
-    const errors = validationResult(req);
-    if(!errors.isEmpty){
-        res.status(401).json({errors : errors.array()})
-    }
- 
-    const {fullName,email,password} = req.body;
+exports.userRegister = async function (req, res, nest) {
+  const errors = validationResult(req);
+  if (!errors.isEmpty) {
+    res.status(401).json({ errors: errors.array() });
+  }
 
-    const hashPassword = await userModel.hashPassword(password);
+  const { fullName, email, password } = req.body;
 
-    const user = await userServices.createUser(
-        fullName.firstName,
-        fullName.lastName,
-        email,
-        hashPassword
-    );
+  const hashPassword = await userModel.hashPassword(password);
 
-    const token = user.generateAuthToken()
-    res.status(201).json({token, user});
+  const user = await userServices.createUser(
+    fullName.firstName,
+    fullName.lastName,
+    email,
+    hashPassword
+  );
 
-
-
-
-
-}
+  const token = user.generateAuthToken();
+  res.status(201).json({ token, user });
+};
