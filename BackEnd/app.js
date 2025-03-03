5e9a64df5983aa917b9392debba3d04e6d0ffecf
raziq0835 <raziq0835@gmail.com>
2025-03-03 21:50:14 +0530

    Tested the user/profile router with auth middleware

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require('cookie-parser')
 const connectToDb = require("./db/db.js");
 const userRouter = require("./routers/user.router.js");
 
@@ -10,6 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
 
 app.get("/", (req, res) => {
   console.log(`hello Uber ${req.method}`);
--- BackEnd/middlewares/user.middleware.js
+++ BackEnd/middlewares/user.middleware.js
@@ -3,13 +3,13 @@ const jwt = require('jsonwebtoken')
 
 
 exports.isUserLoged = async (req,res,next) => {
-    const token = req.cookie.token || req.header('Authorization').split(' ')[1]
+    const token =   (req.header('Authorization') ? req.header('Authorization').split(' ')[1] : null);
 
     if(!token)
         res.status(400).json({messege:"Unauthorized"})
 
     try{
-        const decoded = await jwt.verify(token,process.env.ACCESS_SECRET_KEY)
+        const decoded = jwt.verify(token,process.env.ACCESS_SECRET_KEY)
 
         const user = await userModel.findById(decoded._id)
 
