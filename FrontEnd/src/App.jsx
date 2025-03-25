e676de4bb84e18b1410c05837b5ad99f61c5661a
raziq0835 <raziq0835@gmail.com>
2025-03-25 21:23:57 +0530

    Setup the react routers

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Routers , Route, Switch} from 'react-router-dom';
+import {Routes , Route, Switch} from 'react-router-dom';
 import Home from './pages/Home'
 import UserLogin from './pages/UserLogin'
 import UserSignin from './pages/UserSignin'
@@ -9,13 +9,13 @@ import CaptanSignin from './pages/CaptanSignin'
 const App = () => {
   return (
     <div>
-      <Routers>
-          <Route path='/' element={<home/>} />
+      <Routes>
+          <Route path='/' element={<Home/>} />
           <Route path='/UserLogin' element={<UserLogin/>} />
-          <Route path='./UserSignin' element={<UserSignin/>} />
-          <Route path='./CaptanLogin' element={<CaptanLogin/>} />
-          <Route path='./CaptanSignin' element={<CaptanSignin/>} />
-      </Routers>
+          <Route path='/UserSignin' element={<UserSignin/>} />
+          <Route path='/CaptanLogin' element={<CaptanLogin/>} />
+          <Route path='/CaptanSignin' element={<CaptanSignin/>} />
+      </Routes>
     </div>
   )
 }
--- FrontEnd/src/pages/Home.jsx
+++ FrontEnd/src/pages/Home.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='text-center mt-5 pt-5'>
-      <h1 className='text-center'>Welcome to the Home Page</h1>
+    <div className='text-center text-red-700 mt-5 pt-5'>
+      <h1 className='text-center text-blue-200'>Welcome to the Home Page</h1>
       <p>Home</p>
     </div>
-  )
+  );
 }
 
 export default Home
