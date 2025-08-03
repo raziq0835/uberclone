809e3e155403b1911b1fc2e125f5b25391adbb40
Raziq <raziq0835@gmail.com>
2025-08-03 23:36:45 +0530

    Component folder is created and VehiclePanel is created

---
+++ FrontEnd/src/components/VehiclePanel.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const VehiclePanel = () => {
+  return (
+    <div>
+      <h2>Vehicle Information</h2>
+      <p>Make: Tesla</p>
+      <p>Model: Model S</p>
+      <p>Year: 2020</p>
+    </div>
+  )
+}
+
+export default VehiclePanel
